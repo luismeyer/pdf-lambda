c00e00e55522c52265f58e57f24df8f68886f3d8
luismeyer <41523937+luismeyer@users.noreply.github.com>
2019-07-04 14:03:03 +0200

    feat: local development and more (#1)

--- test-client/index.js
+++ test-client/index.js
@@ -1,12 +1,10 @@
 const fetch = require('node-fetch');
 const fs = require('fs');
 
-const { LAMBDA_ENDPOINT } = require('./.env.js')
-
-var html = fs.readFileSync("./test.html");
+var html = fs.readFileSync('./test-client/test.html');
 const buffer = Buffer.from(html).toString('base64');
 console.info('Sending Htmlfile to Lambda Endpoint...');
-fetch(LAMBDA_ENDPOINT, {
+fetch('http://localhost:4000/pdf', {
   method: 'POST',
   body: JSON.stringify({
     html: buffer,
@@ -16,7 +14,7 @@ fetch(LAMBDA_ENDPOINT, {
 .then( (response) => response.json())
 .then( (response) => {
   console.info('Saving Pdf File...')
-  var wstream = fs.createWriteStream(response.filename);
+  var wstream = fs.createWriteStream(`./test-client/${response.filename}`);
   wstream.write(new Buffer(response.pdf));
   wstream.end();
 });
