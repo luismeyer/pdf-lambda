cff670da12989da920f4485db3aa8c4fec8759ed
Luis Meyer <luis_meyer@outlook.de>
2020-04-17 18:57:29 +0200

    Remove test-client package

--- handler.js
+++ handler.js
@@ -1,19 +1,19 @@
-'use strict';
-const chromium = require('chrome-aws-lambda');
+"use strict";
+const chromium = require("chrome-aws-lambda");
 const chromepath = process.env.IS_OFFLINE && process.env.CHROME_PATH;
 
-const btoa = b => Buffer.from(b, 'base64').toString();
+const btoa = (b) => Buffer.from(b, "base64").toString();
 const CorsHeaders = {
-  'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Credentials': true
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Credentials": true,
 };
 
 const errorResponse = {
   statusCode: 404,
   headers: CorsHeaders,
   body: JSON.stringify({
-    message: 'Error while rendering pdf'
-  })
+    message: "Error while rendering pdf",
+  }),
 };
 
 const successResponse = (pdf, filename) => ({
@@ -21,11 +21,11 @@ const successResponse = (pdf, filename) => ({
   headers: CorsHeaders,
   body: JSON.stringify({
     pdf,
-    filename
-  })
+    filename,
+  }),
 });
 
-module.exports.handler = async event => {
+module.exports.handler = async (event) => {
   const body = JSON.parse(event.body);
   const html = btoa(body.html);
 
@@ -33,24 +33,24 @@ module.exports.handler = async event => {
     args: chromium.args,
     defaultViewport: chromium.defaultViewport,
     executablePath: chromepath ? chromepath : await chromium.executablePath,
-    headless: true
+    headless: true,
   });
 
   try {
-    console.info('Opening HTML File');
+    console.info("Opening HTML File");
     const page = await browser.newPage();
-    await page.goto('data:text/html,' + html, { waitUntil: 'networkidle2' });
-    await page.waitFor('*');
+    await page.goto("data:text/html," + html, { waitUntil: "networkidle2" });
+    await page.waitFor("*");
 
-    console.info('Printing PDF');
-    const buffer = await page.pdf({ format: 'A4' });
+    console.info("Printing PDF");
+    const buffer = await page.pdf({ format: "A4" });
 
-    console.info('Cleanup...');
+    console.info("Cleanup...");
     await browser.close();
 
     return successResponse(buffer, body.filename);
   } catch (e) {
-    console.error('Error while rendering PDF: ', e);
+    console.error("Error while rendering PDF: ", e);
     await browser.close();
     return errorResponse;
   }
--- test-client/index.js
+++ test-client/index.js
@@ -1,27 +1,27 @@
-const fetch = require('node-fetch');
-const fs = require('fs');
-const path = require('path');
-require('dotenv').config();
+const fetch = require("node-fetch");
+const fs = require("fs");
+const path = require("path");
+require("dotenv").config();
 
 const { LAMBDA_ENDPOINT } = process.env;
 if (!LAMBDA_ENDPOINT)
   throw Error('Missing Environment Variable "LAMBDA_ENDPOINT"');
 
-const html = fs.readFileSync(path.join(__dirname, 'test.html'));
-const buffer = Buffer.from(html).toString('base64');
-console.info('Sending Htmlfile to Lambda Endpoint...');
+const html = fs.readFileSync(path.join(__dirname, "test.html"));
+const buffer = Buffer.from(html).toString("base64");
+console.info("Sending Htmlfile to Lambda Endpoint...");
 
 fetch(LAMBDA_ENDPOINT, {
-  method: 'POST',
-  headers: { 'Content-Type': 'application/json' },
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
   body: JSON.stringify({
     html: buffer,
-    filename: 'test.pdf'
-  })
+    filename: "test.pdf",
+  }),
 })
-  .then(response => response.json())
-  .then(response => {
-    console.info('Saving Pdf File...');
+  .then((response) => response.json())
+  .then((response) => {
+    console.info("Saving Pdf File...");
     const wstream = fs.createWriteStream(
       path.join(__dirname, response.filename)
     );
